5ad451f2eaa6b5c3c3718257f5d52ddfddfd78bb
Takuro Ashie <ashie@clear-code.com>
2024-01-24 14:14:42 +0900

    Wayland: Support text_input_unstable_v3 and text_input_unstable_v1
    They are wayland protocols to support input methods:
    https://cgit.freedesktop.org/wayland/wayland-protocols/tree/unstable/text-input/text-input-unstable-v3.xml
    https://cgit.freedesktop.org/wayland/wayland-protocols/tree/unstable/text-input/text-input-unstable-v1.xml
    
    text_input_unstable_v3 is widely supported by major desktop environment
    on GNU/Linux such as GNOME or KDE.
    
    text_input_unstable_v1 isn't so popular but Weston which is the
    reference Wayland implementation supports only it and doesn't support
    text_input_unstable_v3 so that we also implement it.

--- src/wl_init.c
+++ src/wl_init.c
@@ -48,6 +48,8 @@
 #include "pointer-constraints-unstable-v1-client-protocol.h"
 #include "xdg-activation-v1-client-protocol.h"
 #include "idle-inhibit-unstable-v1-client-protocol.h"
+#include "text-input-unstable-v1-client-protocol.h"
+#include "text-input-unstable-v3-client-protocol.h"
 
 // NOTE: Versions of wayland-scanner prior to 1.17.91 named every global array of
 //       wl_interface pointers 'types', making it impossible to combine several unmodified
@@ -84,6 +86,14 @@
 
 #define types _glfw_idle_inhibit_types
 #include "idle-inhibit-unstable-v1-client-protocol-code.h"
+#undef types
+
+#define types _glfw_text_input_v1_types
+#include "text-input-unstable-v1-client-protocol-code.h"
+#undef types
+
+#define types _glfw_text_input_v3_types
+#include "text-input-unstable-v3-client-protocol-code.h"
 #undef types
 
 static void wmBaseHandlePing(void* userData,
@@ -187,6 +197,20 @@ static void registryHandleGlobal(void* userData,
         _glfw.wl.activationManager =
             wl_registry_bind(registry, name,
                              &xdg_activation_v1_interface,
+                             1);
+    }
+    else if (strcmp(interface, "zwp_text_input_manager_v1") == 0)
+    {
+        _glfw.wl.textInputManagerV1 =
+            wl_registry_bind(registry, name,
+                             &zwp_text_input_manager_v1_interface,
+                             1);
+    }
+    else if (strcmp(interface, "zwp_text_input_manager_v3") == 0)
+    {
+        _glfw.wl.textInputManagerV3 =
+            wl_registry_bind(registry, name,
+                             &zwp_text_input_manager_v3_interface,
                              1);
     }
 }
@@ -947,6 +971,10 @@ void _glfwTerminateWayland(void)
         zwp_idle_inhibit_manager_v1_destroy(_glfw.wl.idleInhibitManager);
     if (_glfw.wl.activationManager)
         xdg_activation_v1_destroy(_glfw.wl.activationManager);
+    if (_glfw.wl.textInputManagerV1)
+        zwp_text_input_manager_v1_destroy(_glfw.wl.textInputManagerV1);
+    if (_glfw.wl.textInputManagerV3)
+        zwp_text_input_manager_v3_destroy(_glfw.wl.textInputManagerV3);
     if (_glfw.wl.registry)
         wl_registry_destroy(_glfw.wl.registry);
     if (_glfw.wl.display)
